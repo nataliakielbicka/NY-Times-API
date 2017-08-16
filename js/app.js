40a8ccbe504fd83ca94e5d106111757964527f82
Natalia <natalia.kielbicka@o2.pl>
2017-08-16 15:27:32 +0200

    repair code depending on jslint and add class list as an array

--- js/app.js
+++ js/app.js
@@ -1,63 +1,54 @@
-'use strict'
+(function() {
+    "use strict";
 
-function removeClass(selector, className) {
-    return selector.classList.remove(className);
-}
+    function removeClass(selector, classNames) {
+        return selector.classList.remove(...classNames);
+    }
 
-function addClass(selector, className) {
-    return selector.classList.add(className);
-}
+    function addClass(selector, classNames) {
+        return selector.classList.add(...classNames);
+    }
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var data = JSON.parse(this.responseText),
-            result = "",
-            content = document.getElementById("content"),
-            search = document.getElementById("search"),
-            obj = data.response.docs;
-
-        for (var i = 0; i < obj.length / 100; i++) {
-            result += '<div class="item show">';
-            if (obj[i].multimedia.length > 0) {
-                result += '<a href="' + obj[i].web_url + '" target="_blank"><img src="http://www.nytimes.com/' + obj[i].multimedia[1].url + '" class="thumb"></a>'
-            }
-            result += '<h2 class="article-title"><a href="' + obj[i].web_url + '" target="_blank">' + obj[i].headline.main + '</a></h2>';
-            result += '<p>' + obj[i].snippet + '</p>';
-            result += '<span class="date">' + obj[i].pub_date.substr(0, 10) + '</span>';
-            result += '</div>';
-        }
-        content.innerHTML = result;
-
-        function searchValue() {
-            var searchVal = search.value,
-                searchStr = new RegExp(searchVal, "i"),
-                item = document.getElementsByClassName("item");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText),
+                result = "",
+                content = document.getElementById("content"),
+                search = document.getElementById("search"),
+                obj = data.response.docs;
 
             for (var i = 0; i < obj.length / 100; i++) {
-                removeClass(item[i], 'show')
-                if (obj[i].snippet.toLowerCase().match(searchStr) == searchVal.toLowerCase() || obj[i].headline.main.toLowerCase().match(searchStr) == searchVal.toLowerCase()) {
-                    addClass(item[i], 'show')
+                result += '<div class="item">';
+                if (obj[i].multimedia.length > 0) {
+                    result += '<a href="' + obj[i].web_url + '" target="_blank"><img src="http://www.nytimes.com/' + obj[i].multimedia[1].url + '" class="thumb"></a>';
+                }
+                result += '<h2 class="article-title"><a href="' + obj[i].web_url + '" target="_blank">' + obj[i].headline.main + '</a></h2>';
+                result += '<p>' + obj[i].snippet + '</p>';
+                result += '<span class="date">' + obj[i].pub_date.substr(0, 10) + '</span>';
+                result += '</div>';
+            }
+            content.innerHTML = result;
 
-                    // var old = JSON.stringify(data).replace(/null/g, '"#"'); //convert to JSON string
-                    // var newArray = JSON.parse(old); //convert back to array
-                    // var index = obj[i].snippet.indexOf(searchVal);
+            function searchValue() {
+                var searchVal = search.value,
+                    searchStr = new RegExp(searchVal, "i"),
+                    item = document.getElementsByClassName("item");
 
-                    // if (index >= 0) {
-                    //     obj[i].snippet.replace(obj[i].snippet, obj[i].snippet.substring(0, index) + "<span class='highlight'>" + obj[i].snippet.substring(index, index + searchVal.length) + "</span>" + obj[i].snippet.substring(index + searchVal.length));
-                    //     console.log(obj[i].snippet)
-                    // }
-
+                for (var i = 0; i < obj.length / 100; i++) {
+                    addClass(item[i], ["hidden"]);
+                    if (obj[i].snippet.toLowerCase().match(searchStr) == searchVal.toLowerCase() || obj[i].headline.main.toLowerCase().match(searchStr) == searchVal.toLowerCase()) {
+                        removeClass(item[i], ["hidden"]);
+                    }
                 }
             }
+
+            search.addEventListener("keyup", function() {
+                setTimeout(function() { searchValue(); }, 2000);
+            });
+
         }
-
-        search.addEventListener("keyup", function() {
-            setTimeout(function() { searchValue() }, 1000);
-        });
-
-    }
-};
-//xmlhttp.open("GET", "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=ac6afe457c104a0ba79a894587354ec1", true);
-xmlhttp.open("GET", "https://api.nytimes.com/svc/archive/v1/2017/1.json?api-key=ac6afe457c104a0ba79a894587354ec1", true);
-xmlhttp.send();
+    };
+    xmlhttp.open("GET", "https://api.nytimes.com/svc/archive/v1/2017/1.json?api-key=ac6afe457c104a0ba79a894587354ec1", true);
+    xmlhttp.send();
+}());
